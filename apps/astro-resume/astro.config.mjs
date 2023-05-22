66a73b09e16ccf6d5e0030643515213a325c2d99
SONGHYEON JUN <shjeon0730@gmail.com>
2023-05-22 08:50:01 -0500

    add tailwind

--- apps/astro-resume/astro.config.mjs
+++ apps/astro-resume/astro.config.mjs
@@ -2,7 +2,26 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
-    integrations: [solid(), svelte(), tailwind()],
+    vite: {
+        assetsInclude: ['**/*.svg', '**/*.png'],
+        define: {},
+    },
+    output: 'server',
+    adapter: vercel(),
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        image({
+            cacheDir: '.astro/cache',
+        }),
+        solid(),
+        svelte(),
+    ],
 });
---
+++ apps/astro-resume/tailwind.config.cjs
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            spacing: {
+                '8xl': '96rem',
+                '9xl': '128rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+            backgroundColor: {
+                blue: 'rgb(0, 87, 114)',
+            },
+            borderColor: {
+                'profile-photo': 'rgb(39, 104, 158)',
+            },
+            colors: {
+                title: '#70ad47',
+                desc: 'black',
+                'title-bottom': 'rgb(12, 138, 40)',
+                'section-back': 'rgb(245, 245, 245)',
+
+                'skill-group-back': 'rgb(0, 68, 88)',
+                'skill-gauge-back': 'rgba(187, 187, 187, 0.253)',
+                'right-grid-link': 'rgb(197, 228, 241)',
+                'right-grid-link-hover': 'rgb(238, 233, 143)',
+                'expand-collapse-tool-back': 'rgb(0, 68, 88)',
+                'link-color': 'rgb(0, 68, 88)',
+                'link-hover-color': 'rgb(0, 158, 206)',
+                blue: '#1fb6ff',
+                purple: '#7e5bef',
+                pink: '#ff49db',
+                orange: '#ff7849',
+                green: '#13ce66',
+                yellow: '#ffc82c',
+                'gray-dark': '#273444',
+                gray: '#8492a6',
+                'gray-light': '#d3dce6',
+            },
+            fontFamily: {
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+        },
+    },
+    plugins: [require('@tailwindcss/aspect-ratio')],
+};
