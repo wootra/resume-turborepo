7bfb02b70a7fbf5d0338332d8eaa27e684ea777d
SONGHYEON JUN <shjeon0730@gmail.com>
2023-05-25 01:24:09 -0500

    fix: solid js version of homepage.

--- packages/common-data/data/links.ts
+++ packages/common-data/data/links.ts
@@ -2,6 +2,5 @@ import { LinkMap } from 'resume-types';
 
 export const bannerLinks = Object.freeze({
     'Vanilla JS': 'https://resume-turborepo-resume-html.vercel.app',
-    'Astro JS(Svelte.js, Solid.js)':
-        'https://resume-turborepo-astro-resume.vercel.app',
+    'Astro JS(Solid.js)': 'https://resume-turborepo-astro-resume.vercel.app',
 });
