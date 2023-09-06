a9ffcb959a86aac8a34884a9e10bac0937f41163
SONGHYEON JUN <shjeon0730@gmail.com>
2023-09-05 22:57:16 -0500

    refactor, style

---
+++ apps/astro-resume/src/consts/classes.ts
@@ -0,0 +1,5 @@
+import { twMerge as tw } from 'tailwind-merge';
+
+export const cardClass = tw(
+    'flex items-center justify-center w-32 h-16 rounded-xl shadow-lg bg-slate-200 hover:bg-lime-300 transition-all duration-500 hover:mt-2'
+);
--- apps/web-games/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), svelte()],
-});
