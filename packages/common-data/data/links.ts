109d775d5717afaba23367178a590ea8628afe95
SONGHYEON JUN <shjeon0730@gmail.com>
2023-11-28 21:53:24 -0600

    update  banner

--- packages/common-data/data/links.ts
+++ packages/common-data/data/links.ts
@@ -2,5 +2,5 @@ import { LinkMap } from 'resume-types';
 
 export const bannerLinks = Object.freeze({
     'Vanilla JS': 'https://resume-turborepo-resume-html.vercel.app',
-    'Astro JS(Solid.js)': 'https://resume-turborepo-astro-resume.vercel.app',
+    'Astro JS(React.js)': 'https://resume-turborepo-astro-resume.vercel.app',
 });
