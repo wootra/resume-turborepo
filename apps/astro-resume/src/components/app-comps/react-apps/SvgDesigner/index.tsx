51f448904cb717f6dcd2b7a6ae7e7885c35948b0
SONGHYEON JUN <shjeon0730@gmail.com>
2023-11-30 17:49:13 -0600

    update svg-designer link

--- apps/astro-resume/src/components/app-comps/react-apps/SvgDesigner/index.tsx
+++ apps/astro-resume/src/components/app-comps/react-apps/SvgDesigner/index.tsx
@@ -1,3 +1,17 @@
-import SvgDesigner from 'svg-designer';
+const SvgDesigner = () => {
+    return (
+        <div>
+            <h1>SVG Designer</h1>
+            <p>SVG Designer is a React component for creating SVGs.</p>
+            <p>
+                See the{' '}
+                <a href='https://svg-designer-snowy.vercel.app' target='_blank'>
+                    SVG Designer
+                </a>{' '}
+                page for more information.
+            </p>
+        </div>
+    );
+};
 
 export default SvgDesigner;
