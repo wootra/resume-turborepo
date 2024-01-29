6870c2afccb7748fc2b5e84ecdfa43386862cbfa
Songhyeon Jun <shjeon0730@gmail.com>
2024-01-28 23:01:38 -0600

    update package.json for react-svg-chart

--- packages/react-svg-chart/src/PieChart/index.tsx
+++ packages/react-svg-chart/src/PieChart/index.tsx
@@ -10,6 +10,11 @@ type PieDataAngle = {
     percentage: number;
 };
 
+type PieChartAutoPositions = {
+    xAutoPos?: 'center' | 'left' | 'right';
+    yAutoPos?: 'center' | 'top' | 'bottom';
+};
+
 type PieData =
     | {
           type: 'percentage';
@@ -20,16 +25,26 @@ type PieData =
           data: PieDataAngle[];
       };
 
-type Props = PieData & {
-    width: number;
-    height: number;
-    xRotate?: number;
-};
+type Props = PieData &
+    PieChartAutoPositions & {
+        width: number;
+        height: number;
+        xRotate?: number;
+    };
 
-export const PieChart = ({ width, height, xRotate = 0, type, data }: Props) => {
+export const PieChart = ({
+    width,
+    height,
+    xRotate = 0,
+    type,
+    data,
+    xAutoPos,
+    yAutoPos,
+}: Props) => {
+    const pos = {};
     return (
         <svg width={width} height={height}>
-            <svg viewBox='0 0 100 100' width='100%' height='100%'>
+            <svg viewBox='0 0 100 100' preserveAspectRatio='xMaxYMin meet'>
                 <path d='M50 50 L50 10 A40 40 0 0 1 90 50 Z' fill='red' />
                 <path d='M50 50 L90 50 A40 40 0 0 1 50 90 Z' fill='blue' />
                 <path d='M50 50 L50 90 A40 40 0 0 1 10 50 Z' fill='green' />
