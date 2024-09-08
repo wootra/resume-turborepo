e3d8ce3d7d849e21d67370a31e61de4fa6bde25a
Songhyeon Jun <shjeon0730@gmail.com>
2024-09-07 21:06:24 -0500

    fix ball speed bug

--- apps/astro-resume/src/components/entry/GamificBackground.tsx
+++ apps/astro-resume/src/components/entry/GamificBackground.tsx
@@ -2,163 +2,147 @@ import React, { useEffect, useRef, useState, useCallback } from 'react';
 import * as d3 from 'd3';
 import { checkVisible } from '@/utils/checkVisible';
 type Pos = {
-    key: string;
-    x: number;
-    y: number;
-    colorBase: number;
-    color: string;
-    size: number;
+	key: string;
+	x: number;
+	y: number;
+	colorBase: number;
+	color: string;
+	size: number;
 };
 const NUM_OF_BOXES = 10;
 const OPEN_RADIUS = NUM_OF_BOXES * 0.5;
 const init = (setPoints: React.Dispatch<React.SetStateAction<Pos[]>>) => {
-    const rows = Array(NUM_OF_BOXES)
-        .fill(null)
-        .map((_, i) => {
-            return Array(NUM_OF_BOXES)
-                .fill(null)
-                .map((_, j) => {
-                    const colorBase = Math.random() * 360;
-                    const d = {
-                        key: `${i}-${j}`,
-                        x: i,
-                        y: j,
-                        size: 1,
-                        colorBase,
-                        color: `hsl(${colorBase}, 100%, 100%)`,
-                    };
+	const rows = Array(NUM_OF_BOXES)
+		.fill(null)
+		.map((_, i) => {
+			return Array(NUM_OF_BOXES)
+				.fill(null)
+				.map((_, j) => {
+					const colorBase = Math.random() * 360;
+					const d = {
+						key: `${i}-${j}`,
+						x: i,
+						y: j,
+						size: 1,
+						colorBase,
+						color: `hsl(${colorBase}, 100%, 100%)`,
+					};
 
-                    return d;
-                });
-        })
-        .flat();
-    return rows;
+					return d;
+				});
+		})
+		.flat();
+	setPoints(rows);
 };
 
 const GamificBackground = () => {
-    const ref = useRef<HTMLDivElement | null>(null);
-    const [points, setPoints] = useState<Pos[]>([]);
-    const pointLen = useRef<number>(0);
-    pointLen.current = points.length;
-    useEffect(() => {
-        // if (ref.current) {
-        if (pointLen.current === 0) {
-            setPoints(init(setPoints));
-        }
-        setPoints(init(setPoints));
-        const handler = () => {
-            if (!checkVisible(ref.current)) {
-                if (pointLen.current > 0) {
-                    setPoints([]);
-                }
-                return;
-            } else {
-                if (pointLen.current === 0) {
-                    setPoints(init(setPoints));
-                }
-            }
-        };
-        document.addEventListener('scrollend', handler);
-        return () => {
-            document.removeEventListener('scrollend', handler);
-        };
-        // }
-    }, []);
-    const onMouseMove: React.MouseEventHandler<HTMLDivElement> = useCallback(
-        e => {
-            if (ref.current) {
-                if (!checkVisible(ref.current)) {
-                    console.count('ignored!');
-                    return;
-                }
-                const [px, py] = d3.pointer(e);
+	const ref = useRef<HTMLDivElement | null>(null);
+	const [points, setPoints] = useState<Pos[]>([]);
+	const pointLen = useRef<number>(0);
+	pointLen.current = points.length;
+	useEffect(() => {
+		// if (ref.current) {
+		if (pointLen.current === 0) {
+			init(setPoints);
+		}
+		init(setPoints);
+		const handler = () => {
+			if (!checkVisible(ref.current)) {
+				if (pointLen.current > 0) {
+					setPoints([]);
+				}
+				return;
+			} else {
+				if (pointLen.current === 0) {
+					init(setPoints);
+				}
+			}
+		};
+		document.addEventListener('scrollend', handler);
+		return () => {
+			document.removeEventListener('scrollend', handler);
+		};
+		// }
+	}, []);
+	const onMouseMove: React.MouseEventHandler<HTMLDivElement> = useCallback(
+		e => {
+			if (ref.current) {
+				if (!checkVisible(ref.current)) {
+					console.count('ignored!');
+					return;
+				}
+				const [px, py] = d3.pointer(e);
 
-                const element = ref.current;
-                const wid = element.clientWidth;
-                const hig = element.clientHeight;
-                const widScale = d3.scaleLinear([0, wid], [0, NUM_OF_BOXES]);
-                const higScale = d3.scaleLinear([0, hig], [0, NUM_OF_BOXES]);
-                const [sx, sy] = [widScale(px), higScale(py)];
-                const newPoints: Pos[] = points.map(pos => {
-                    const { x, y, size, colorBase } = pos;
-                    const nSize = Math.min(
-                        1,
-                        Math.sqrt(
-                            Math.pow(Math.abs(x - sx), 2) +
-                                Math.pow(Math.abs(y - sy), 2)
-                        ) / OPEN_RADIUS
-                    );
+				const element = ref.current;
+				const wid = element.clientWidth;
+				const hig = element.clientHeight;
+				const widScale = d3.scaleLinear([0, wid], [0, NUM_OF_BOXES]);
+				const higScale = d3.scaleLinear([0, hig], [0, NUM_OF_BOXES]);
+				const [sx, sy] = [widScale(px), higScale(py)];
+				const newPoints: Pos[] = points.map(pos => {
+					const { x, y, size, colorBase } = pos;
+					const nSize = Math.min(
+						1,
+						Math.sqrt(Math.pow(Math.abs(x - sx), 2) + Math.pow(Math.abs(y - sy), 2)) / OPEN_RADIUS
+					);
 
-                    if (nSize !== size) {
-                        return {
-                            ...pos,
-                            color:
-                                nSize === 1
-                                    ? '#ffffff'
-                                    : `hsl(${colorBase}, ${100 * size}%, ${
-                                          100 * size
-                                      }%)`,
-                            size: nSize,
-                        };
-                    }
+					if (nSize !== size) {
+						return {
+							...pos,
+							color: nSize === 1 ? '#ffffff' : `hsl(${colorBase}, ${100 * size}%, ${100 * size}%)`,
+							size: nSize,
+						};
+					}
 
-                    return pos;
-                });
+					return pos;
+				});
 
-                setPoints(newPoints);
-            }
-        },
-        [points]
-    );
+				setPoints(newPoints);
+			}
+		},
+		[points]
+	);
 
-    return (
-        <div
-            ref={ref}
-            className='absolute inset-0 z-50 flex items-center justify-center'
-            onMouseMoveCapture={onMouseMove}
-        >
-            <svg
-                className='w-full rounded-full'
-                viewBox={`0 0 ${NUM_OF_BOXES} ${NUM_OF_BOXES}`}
-            >
-                {points.map(d => {
-                    let size = d.size === 1 ? 1 : d.size * d.size * d.size;
-                    if (size < 0.1) size = 0;
-                    const x = d.size === 1 ? d.x : d.x + 1 / 2 - size / 2;
-                    const y = d.size === 1 ? d.y : d.y + 1 / 2 - size / 2;
+	return (
+		<div
+			ref={ref}
+			className='absolute inset-0 z-50 flex items-center justify-center'
+			onMouseMoveCapture={onMouseMove}
+		>
+			<svg className='w-full rounded-full' viewBox={`0 0 ${NUM_OF_BOXES} ${NUM_OF_BOXES}`}>
+				{points.map(d => {
+					let size = d.size === 1 ? 1 : d.size * d.size * d.size;
+					if (size < 0.1) size = 0;
+					const x = d.size === 1 ? d.x : d.x + 1 / 2 - size / 2;
+					const y = d.size === 1 ? d.y : d.y + 1 / 2 - size / 2;
 
-                    return (
-                        <g
-                            key={d.key}
-                            transform={`translate(${x + 1 / 2},${y + 1 / 2})`}
-                        >
-                            <rect
-                                x={-0.5}
-                                y={-0.5}
-                                rx={size === 1 ? 0 : 0.5 - size / 2}
-                                ry={size === 1 ? 0 : 0.5 - size / 2}
-                                style={
-                                    size === 1
-                                        ? {}
-                                        : {
-                                              transform: `rotate(${(
-                                                  -90 * d.size
-                                              ).toFixed(2)}deg)`,
-                                          }
-                                }
-                                width={size * 1.1}
-                                height={size * 1.1}
-                                fill={d.color}
-                                strokeWidth={0}
-                                // stroke={d.color}
-                                // strokeWidth={size}
-                            />
-                        </g>
-                    );
-                })}
-            </svg>
-        </div>
-    );
+					return (
+						<g key={d.key} transform={`translate(${x + 1 / 2},${y + 1 / 2})`}>
+							<rect
+								x={-0.5}
+								y={-0.5}
+								rx={size === 1 ? 0 : 0.5 - size / 2}
+								ry={size === 1 ? 0 : 0.5 - size / 2}
+								style={
+									size === 1
+										? {}
+										: {
+												transform: `rotate(${(-90 * d.size).toFixed(2)}deg)`,
+										  }
+								}
+								width={size * 1.1}
+								height={size * 1.1}
+								fill={d.color}
+								strokeWidth={0}
+								// stroke={d.color}
+								// strokeWidth={size}
+							/>
+						</g>
+					);
+				})}
+			</svg>
+		</div>
+	);
 };
 
 export default GamificBackground;
--- apps/astro-resume/src/components/entry/GamificBackgroundPulse.tsx
+++ apps/astro-resume/src/components/entry/GamificBackgroundPulse.tsx
@@ -1,192 +1,167 @@
 import { checkVisible } from '@/utils/checkVisible';
-import React, {
-    useCallback,
-    useEffect,
-    useRef,
-    type RefObject,
-    useState,
-} from 'react';
+import React, { useCallback, useEffect, useRef, type RefObject, useState } from 'react';
 import * as d3 from 'd3';
 type BallInfo = {
-    xSpeed: number;
-    ySpeed: number;
-    x: number;
-    y: number;
+	xSpeed: number;
+	ySpeed: number;
+	x: number;
+	y: number;
 };
 type Scales = {
-    xScale: d3.ScaleLinear<number, number>;
-    yScale: d3.ScaleLinear<number, number>;
-    w: number;
-    h: number;
+	xScale: d3.ScaleLinear<number, number>;
+	yScale: d3.ScaleLinear<number, number>;
+	w: number;
+	h: number;
 };
 const FRAME_LEN = 20;
-const INIT_YACC = 0.09;
+const INIT_YACC = 0.2;
 const SPEED_RESOLUTION = 0.01;
 const MARGIN = 4;
 const getScales = (ref: RefObject<HTMLDivElement | null>): Scales => {
-    if (ref.current) {
-        const size = {
-            w: ref.current.clientWidth - MARGIN * 2,
-            h: ref.current.clientHeight - MARGIN * 2,
-        };
-        if (size.w > size.h) {
-            return {
-                xScale: d3.scaleLinear([0, 100], [MARGIN, size.w]),
-                yScale: d3.scaleLinear(
-                    [0, (100 * size.h) / size.w],
-                    [MARGIN, size.h]
-                ),
-                w: 100,
-                h: (100 * size.h) / size.w,
-            };
-        } else {
-            return {
-                xScale: d3.scaleLinear(
-                    [0, (100 * size.w) / size.h],
-                    [MARGIN, size.w]
-                ),
-                yScale: d3.scaleLinear([0, 100], [MARGIN, size.h]),
-                w: (100 * size.w) / size.h,
-                h: 100,
-            };
-        }
-    }
-    throw new Error('ref cannot be null');
+	if (ref.current) {
+		const size = {
+			w: ref.current.clientWidth - MARGIN * 2,
+			h: ref.current.clientHeight - MARGIN * 2,
+		};
+		if (size.w > size.h) {
+			return {
+				xScale: d3.scaleLinear([0, 100], [MARGIN, size.w]),
+				yScale: d3.scaleLinear([0, (100 * size.h) / size.w], [MARGIN, size.h]),
+				w: 100,
+				h: (100 * size.h) / size.w,
+			};
+		} else {
+			return {
+				xScale: d3.scaleLinear([0, (100 * size.w) / size.h], [MARGIN, size.w]),
+				yScale: d3.scaleLinear([0, 100], [MARGIN, size.h]),
+				w: (100 * size.w) / size.h,
+				h: 100,
+			};
+		}
+	}
+	throw new Error('ref cannot be null');
 };
 
-const getNextBallInfo = (
-    prevBallInfo: BallInfo,
-    timeDiff: number,
-    acc: { x: number; y: number },
-    scales: Scales
-) => {
-    const { xScale, yScale, w, h } = scales;
+const getNextBallInfo = (prevBallInfo: BallInfo, timeDiff: number, acc: { x: number; y: number }, scales: Scales) => {
+	const { xScale, yScale, w, h } = scales;
 
-    const { xSpeed, ySpeed, x, y } = prevBallInfo;
-    const x2 = x + xSpeed * timeDiff * SPEED_RESOLUTION;
-    const y2 = y + ySpeed * timeDiff * SPEED_RESOLUTION;
-    const xDir = x2 > w || x2 < -1 ? -1 : 1;
-    const yDir = y2 > h || y2 < -1 ? -1 : 1;
-    const ballInfo = {
-        x:
-            xDir > 0
-                ? x2 // direction same
-                : x2 < 0
-                  ? 0
-                  : w,
-        y: yDir > 0 ? y2 : y2 < 0 ? 0 : h,
-        xSpeed: (xSpeed + acc.x * timeDiff * SPEED_RESOLUTION) * xDir,
-        ySpeed: (ySpeed + acc.y * timeDiff * SPEED_RESOLUTION) * yDir,
-    };
-    return ballInfo;
+	const { xSpeed, ySpeed, x, y } = prevBallInfo;
+	const x2 = x + xSpeed * timeDiff * SPEED_RESOLUTION;
+	const y2 = y + ySpeed * timeDiff * SPEED_RESOLUTION;
+	const xSDir = xSpeed > 0 ? 1 : -1;
+	const ySDir = ySpeed > 0 ? 1 : -1;
+	const xDir = x2 > w || x2 < -1 ? -1 : 1;
+	const yDir = y2 > h || y2 < -1 ? -1 : 1;
+	const ballInfo = {
+		x:
+			xDir > 0
+				? x2 // direction same
+				: x2 < 0
+				  ? 0
+				  : w,
+		y: yDir > 0 ? y2 : y2 < 0 ? 0 : h,
+		xSpeed: (Math.min(Math.abs(xSpeed), 40) * xSDir + acc.x * timeDiff * SPEED_RESOLUTION) * xDir,
+		ySpeed: (Math.min(Math.abs(ySpeed), 40) * ySDir + acc.y * timeDiff * SPEED_RESOLUTION) * yDir,
+	};
+	return ballInfo;
 };
 
 const GamificBackgroundPulse = () => {
-    const containerRef = useRef<HTMLDivElement | null>(null);
-    const animationRef = useRef<boolean>(false);
-    const [isAnimnationOn, setIsAnimationOn] = useState(true);
-    const isAnimnationOnRef = useRef<boolean>(isAnimnationOn);
-    isAnimnationOnRef.current = isAnimnationOn;
-    const prevFrameTime = useRef<number>(0);
-    const ballRef = useRef<HTMLDivElement | null>(null);
-    const ballInfoRef = useRef<BallInfo>({
-        xSpeed: 1,
-        ySpeed: 1,
-        x: 0,
-        y: 0,
-    });
-    const acc = useRef<{ x: number; y: number }>({ x: 0, y: INIT_YACC });
-    const scalesRef = useRef<Scales | null>(null);
-    const pulseCb = useCallback((frame: number) => {
-        if (animationRef.current && containerRef.current && scalesRef.current) {
-            const timeDiff = frame - prevFrameTime.current;
-            if (timeDiff > FRAME_LEN) {
-                ballInfoRef.current = getNextBallInfo(
-                    ballInfoRef.current,
-                    timeDiff,
-                    acc.current,
-                    scalesRef.current
-                );
-                if (ballRef.current && isAnimnationOnRef.current) {
-                    const { xScale, yScale } = scalesRef.current;
-                    ballRef.current.setAttribute(
-                        'style',
-                        `left: ${xScale(
-                            ballInfoRef.current.x
-                        )}px; top: ${yScale(ballInfoRef.current.y)}px`
-                    );
-                }
-                prevFrameTime.current = frame;
-            }
-            window.requestAnimationFrame(pulseCb);
-        }
-    }, []);
-    useEffect(() => {
-        const resizeObserver = new ResizeObserver(entries => {
-            for (const entry of entries) {
-                if (entry.contentBoxSize) {
-                    const contentBoxSize = entry.contentBoxSize[0];
-                    contentBoxSize.inlineSize;
-                    const scales = getScales(containerRef);
-                    scalesRef.current = scales;
-                }
-            }
-        });
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const animationRef = useRef<boolean>(false);
+	const [isAnimnationOn, setIsAnimationOn] = useState(true);
+	const isAnimnationOnRef = useRef<boolean>(isAnimnationOn);
+	isAnimnationOnRef.current = isAnimnationOn;
+	const prevFrameTime = useRef<number>(0);
+	const ballRef = useRef<HTMLDivElement | null>(null);
+	const ballInfoRef = useRef<BallInfo>({
+		xSpeed: 1,
+		ySpeed: 1,
+		x: 0,
+		y: 0,
+	});
+	const acc = useRef<{ x: number; y: number }>({ x: 0, y: INIT_YACC });
+	const scalesRef = useRef<Scales | null>(null);
+	const pulseCb = useCallback((frame: number) => {
+		if (animationRef.current && containerRef.current && scalesRef.current) {
+			const timeDiff = frame - prevFrameTime.current;
+			if (timeDiff > FRAME_LEN) {
+				ballInfoRef.current = getNextBallInfo(ballInfoRef.current, timeDiff, acc.current, scalesRef.current);
+				if (ballRef.current && isAnimnationOnRef.current) {
+					const { xScale, yScale } = scalesRef.current;
+					ballRef.current.setAttribute(
+						'style',
+						`left: ${xScale(ballInfoRef.current.x)}px; top: ${yScale(ballInfoRef.current.y)}px`
+					);
+				}
+				prevFrameTime.current = frame;
+			}
+			window.requestAnimationFrame(pulseCb);
+		}
+	}, []);
+	useEffect(() => {
+		const resizeObserver = new ResizeObserver(entries => {
+			for (const entry of entries) {
+				if (entry.contentBoxSize) {
+					const contentBoxSize = entry.contentBoxSize[0];
+					contentBoxSize.inlineSize;
+					const scales = getScales(containerRef);
+					scalesRef.current = scales;
+				}
+			}
+		});
 
-        const handler = () => {
-            if (!checkVisible(containerRef.current)) {
-                if (animationRef.current) {
-                    animationRef.current = false;
-                    resizeObserver.disconnect();
-                }
-                return;
-            } else {
-                if (!animationRef.current) {
-                    animationRef.current = true;
-                    window.requestAnimationFrame(pulseCb);
-                    ballInfoRef.current = {
-                        xSpeed: 1,
-                        ySpeed: 1,
-                        x: 0,
-                        y: 0,
-                    };
-                    if (containerRef.current) {
-                        const scales = getScales(containerRef);
-                        scalesRef.current = scales;
-                        resizeObserver.observe(containerRef.current);
-                    }
-                }
-            }
-        };
-        if (containerRef.current) {
-            const scales = getScales(containerRef);
-            scalesRef.current = scales;
-        }
-        document.addEventListener('scrollend', handler);
-        return () => {
-            resizeObserver.disconnect();
-            document.removeEventListener('scrollend', handler);
-        };
-    }, []);
-    return (
-        <>
-            <button
-                onClick={() => setIsAnimationOn(state => !state)}
-                className='absolute w-12 h-8 z-50 left-2 top-2 bg-red-200 text-black rounded-md hover:bg-red-50'
-            >
-                {isAnimnationOn ? 'OFF' : 'ON'}
-            </button>
-            <div
-                className='absolute inset-0 pointer-events-none'
-                ref={containerRef}
-            >
-                <div
-                    ref={ballRef}
-                    className='bg-gradient-to-br from-slate-100 to-lime-900 w-4 h-4 absolute rounded-full z-50 animate-size-pulse'
-                />
-            </div>
-        </>
-    );
+		const handler = () => {
+			if (!checkVisible(containerRef.current)) {
+				if (animationRef.current) {
+					animationRef.current = false;
+					resizeObserver.disconnect();
+				}
+				return;
+			} else {
+				if (!animationRef.current) {
+					animationRef.current = true;
+					window.requestAnimationFrame(pulseCb);
+					ballInfoRef.current = {
+						xSpeed: 1,
+						ySpeed: 1,
+						x: 0,
+						y: 0,
+					};
+					if (containerRef.current) {
+						const scales = getScales(containerRef);
+						scalesRef.current = scales;
+						resizeObserver.observe(containerRef.current);
+					}
+				}
+			}
+		};
+		if (containerRef.current) {
+			const scales = getScales(containerRef);
+			scalesRef.current = scales;
+		}
+		document.addEventListener('scrollend', handler);
+		return () => {
+			resizeObserver.disconnect();
+			document.removeEventListener('scrollend', handler);
+		};
+	}, []);
+	return (
+		<>
+			<button
+				onClick={() => setIsAnimationOn(state => !state)}
+				className='absolute w-12 h-8 z-50 left-2 top-2 bg-red-200 text-black rounded-md hover:bg-red-50'
+			>
+				{isAnimnationOn ? 'OFF' : 'ON'}
+			</button>
+			<div className='absolute inset-0 pointer-events-none border-2 border-green' ref={containerRef}>
+				<div
+					ref={ballRef}
+					className='bg-gradient-to-br from-slate-100 to-lime-900 w-4 h-4 absolute rounded-full z-50 animate-size-pulse'
+				/>
+			</div>
+		</>
+	);
 };
 
 export default GamificBackgroundPulse;
