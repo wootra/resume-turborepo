9665157ee2d9e1a2feb467f29872f6175581857e
Songhyeon Jun <shjeon0730@gmail.com>
2026-02-23 23:20:25 -0600

    update url

--- apps/astro-utils/astro.config.mjs
+++ apps/astro-utils/astro.config.mjs
@@ -13,7 +13,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	site: 'https://utils.sh-jun.com',
+	site: 'https://resume-utils.sh-jun.com',
 	adapter: vercel({
 		// imagesConfig: {
 		//     cacheDir: '.astro/cache',
